5dbd9249de8e26741f7020c1d6b2227ec43e2099
melnikova <melnikova@MELNIKOvA>
2018-11-23 10:10:21 +0000

    some modifications

--- moment.py
+++ moment.py
@@ -5,6 +5,7 @@ from gdal_helper import Vec2
 from math import copysign
 
 class Moment:
+    moment_count = 0
     HU_DICT = {
         1: lambda f: f(2, 0) + f(0, 2),
         2: lambda f: (f(2, 0) - f(0, 2)) ** 2 + 4 * f(1, 1) ** 2,
@@ -20,6 +21,7 @@ class Moment:
     }
 
     def __init__(self, geom: 'GDAL Polygon'):
+        self.moment_count+=1
         if type(geom) != ogr.Geometry:
             print("Invalid geometry. Geometry type must be ogr.wkbPolygon")
             return
@@ -32,7 +34,9 @@ class Moment:
             for i in range(1, ring.GetPointCount()):
                 v1 = Vec2(ring.GetPoint(i-1)[:-1])
                 v2 = Vec2(ring.GetPoint(i)[:-1])
-                segments.append([v1, v2])
+                if v1 != v2:
+                    segments.append([v1, v2])
+
         self.segments = segments
 
     @contract
@@ -53,41 +57,43 @@ class Moment:
         av_x = 0
         av_y = 0
 
-        if central:
+        if (central):
             m00 = self.compute(0, 0, central=False, scale_inv=False)
             m01 = self.compute(0, 1, central=False, scale_inv=False)
             m10 = self.compute(1, 0, central=False, scale_inv=False)
             av_x += -m10 / m00
             av_y += -m01 / m00
 
+
         for s in self.segments:
             dx = (s[1].x - s[0].x)
             dy = (s[1].y - s[0].y)
 
-            if dx!=0:
-                if dy!=0:sign_l = copysign(1, dy/dx)
-                else: sign_l = copysign(1, dx)
-            else:
-                sign_l = copysign(1, dy)
-
-            length = sign_l*((s[1] - s[0]).length())
+            length = ((s[1] - s[0]).length())
             k1 = dx / length
             k2 = dy / length
+
             b1 = av_x + s[0].x
             b2 = av_y + s[0].y
-            m = self.compute_segment_moment(i, j, length, k1, b1, k2, b2)
+
+            k3 = dx * dy / length**2
+            b3 = s[0].y*dx/length
+
+            m = self.compute_segment_moment(i, j, length, k1, b1, k2, b2, k3, b3)
+
             m_result += m
+
         if scale_inv:
-            m_result = m_result/(m00**((i+j)/2+1))
+            m_result = m_result/(m00**((i+j)+1))
         return m_result
 
 
     @staticmethod
-    def compute_segment_moment(i, j, length, k1, b1, k2, b2):
+    def compute_segment_moment(i, j, length, k1, b1, k2, b2, k3, b3):
         a = Polynome.binomial_theorem(x_coef=k1, y_coef=b1, power=i)
         b = Polynome.binomial_theorem(x_coef=k2, y_coef=b2, power=j)
-        c = Polynome.binomial_theorem(x_coef=1, y_coef=0, power=1)
-        result = a * b *c
+        # c = Polynome.binomial_theorem(x_coef=k3, y_coef=b3, power=1)
+        result = a * b
         m = result.compute_integral_x(x_begin=0, x_end=length, y_value=1)
         return m
 
